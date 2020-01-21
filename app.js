df4fd736e48a522f4784af7fdd843aace34f3d28
Trenton Brew <tbrew1023@gmail.com>
2020-01-21 06:27:35 -0600

    progress

---
+++ app.js
@@ -0,0 +1,216 @@
+// Change the configurations.  
+var config = {
+    apiKey: "AIzaSyCHpz4ty7srkDV3AiUDZJLFEOfYGLMpqUM",
+    authDomain: "nihal-819a6.firebaseapp.com",
+    databaseURL: "https://nihal-819a6.firebaseio.com",
+    projectId: "nihal-819a6",
+    storageBucket: "nihal-819a6.appspot.com",
+    messagingSenderId: "489064704671"
+}
+    
+// Initialize Firebase.
+firebase.initializeApp(config);
+
+const firestore = firebase.firestore();
+const settings = {/* your settings... */ timestampsInSnapshots: true};
+firestore.settings(settings);
+
+new Vue({
+    el: "#app",
+    firestore() {
+        return {
+            items: firebase.firestore().collection("portfolioItems")
+        }
+    },
+    data(){
+        var creditNum = 1
+        return {
+            options: {
+                afterLoad: this.handleLoad,
+                navigation: true,
+                navigationPosition: 'right',
+                parallax: true
+            },
+            item: {
+                title: "",
+                genre: "",
+                summary: "",
+                finalFilm: "",
+                credits: [
+                    {
+                        role: "",
+                        name: ""
+                    }
+                ],
+                screenGrabs: [
+                    ""
+                ]
+            }
+        }
+    },
+    mounted: function() {
+
+        //----- Loading component and initializing fullpage -----
+
+        var parentObj = this
+        //console.log(this.items.length);
+        setTimeout(function() {
+            console.log("item count: " + parentObj.items.length);
+            parentObj.componentsReady();
+        }, 1500)
+    },
+    methods: {
+        componentsReady() {
+            this.$refs.fullpage.init()
+            this.$refs.fullpage.build()
+            console.log('fullpage initialized');
+        },
+        add() {
+            console.log('clicked')
+            this.$firestore.items.add(this.item).then(()=>{
+                this.item.title = "",
+                this.item.genre = "",
+                this.item.summary = "",
+                this.item.finalFilm = "",
+                this.item.credits = [
+                    {
+                        role: "",
+                        name: ""
+                    }
+                ],
+                this.item.screenGrabs = [
+                    ""
+                ]
+            })
+
+            //refresh page
+            document.location.reload();
+        },
+        remove(e) {
+            console.log(e.screenGrabs);
+
+            //deleting screenGrab directory in storage
+
+
+            /* ---------- TODO ---------- */
+
+
+            /*for(let i = 0; i < e.screenGrabs.length; i++) {
+                firebase.storage().ref().child(e.imgPaths[i]).delete().then(function() {
+                    console.log("Image deleted successfully");
+                }).catch(function(error) {
+                    console.error("Image not deleted successfully");
+                });
+            }*/
+
+            //deleting item from database
+            this.$firestore.items.doc(e['.key']).delete().then(
+                function() {
+                    //refresh page
+                    document.location.reload();       
+                }
+            )
+        },
+        newCredit() {
+            this.item.credits.push(
+                {
+                    role: "",
+                    name: ""
+                }
+            );
+        },
+        newScreenGrab() {
+            //console.log("pushed new screengrab")
+        },
+        handleUpload(e) {
+            console.log("...handling screengrab upload for " + this.item.title);
+
+            var parentObj = this;
+
+            var file = e.target.files[0];
+
+            var imgPath = this.item.title + "/" + file.name;
+
+            //create a storage ref
+            var storageRef = firebase.storage().ref(imgPath);
+
+            //upload file
+            var task = storageRef.put(file);
+
+            //update progress bar
+            task.on('state_changed', 
+            
+                function progress(snapshot) {
+                    //var percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                   //uploader.value = percentage;
+                },
+                function error(err) {
+                    
+                },
+                function complete() {
+                    //console.log(storageRef.child(imgPath).getDownloadURL().getResults());
+
+                    task.snapshot.ref.getDownloadURL().then(
+                        function(downloadURL) {
+                            console.log('File available at: ' + downloadURL)
+                            parentObj.item.screenGrabs.push(
+                                downloadURL + ""
+                            );
+                            console.log('screenGrabs' + parentObj.item.screenGrabs);
+                        }
+                    )
+
+                    //console.log("screenGrabs: " + parentObj.item.screenGrabs);
+                }
+            
+            )
+
+        },
+        handleFilmUpload(e) {
+            console.log("...handling film upload for " + this.item.title);
+
+            var parentObj = this;
+
+            var file = e.target.files[0];
+
+            var filmPath = this.item.title + "/" + file.name;
+
+            //create a storage ref
+            var storageRef = firebase.storage().ref(filmPath);
+
+            //upload file
+            var task = storageRef.put(file);
+
+            //update progress bar
+            task.on('state_changed', 
+            
+                function progress(snapshot) {
+                    var percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                    uploader.value = percentage;
+
+                    if(percentage == 100) {
+                        uploader.style.background = "#66BB6A";
+                    }
+                },
+                function error(err) {
+                    
+                },
+                function complete() {
+                    //console.log(storageRef.child(imgPath).getDownloadURL().getResults());
+
+                    task.snapshot.ref.getDownloadURL().then(
+                        function(downloadURL) {
+                            console.log('File available at: ' + downloadURL)
+                            parentObj.item.finalFilm = downloadURL + ""
+                        }
+                    )
+
+                    console.log("video: " + parentObj.item.screenGrabs);
+                }
+            )
+        },
+        handleLoad() {
+            console.log("Emitted 'after load' event");
+        }
+    }
+})
